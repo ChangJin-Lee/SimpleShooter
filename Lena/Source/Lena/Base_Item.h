2a620289d3a2fe014ad8fb7486763d61f8ea7047
ChangJin-Lee <ckdwls525@gmail.com>
2024-06-27 00:23:26 +0900

    feat: Inventory System

---
+++ Lena/Source/Lena/Base_Item.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Base_Item.generated.h"
+
+UCLASS()
+class LENA_API ABase_Item : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABase_Item();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	FName ItemName;
+};
